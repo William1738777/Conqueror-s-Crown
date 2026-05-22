0870cbce68de11533f07493104391607125f838f
William1738777 <sparringbible@gmail.com>
2026-05-23 01:03:47 +0800

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1784,6 +1784,7 @@ async function processQueue(sideProcessing, queueArr) {
         } 
         else { if (typeof triggerLicenseQuest === 'function') triggerLicenseQuest(); }
     }
+}
 
 
 document.getElementById('exec-btn').addEventListener('click', () => { 
